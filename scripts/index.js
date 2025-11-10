70a2c74fad19ee155bc4b2fcc51ec34d172b9e21
Enxos27 <vincenzo.calvaruso17@gmail.com>
2025-11-10 17:37:39 +0100

    ref

--- scripts/index.js
+++ scripts/index.js
@@ -33,19 +33,16 @@ const addAlbumHomepage = function () {
         const div = document.createElement("div");
 
         div.innerHTML = `
-             
-              <div class="card alburm-card bg-dark h-100 ">
-                <img src="${arrayOfAlbum.cover_medium}" class="card-img-top flex-grow-1 imgAlbum p-2 object-fit-cover rounded-3" alt="Cover Album"  />
+              <div class="card alburm-card bg-dark maxWidth mx-auto my-2">
+                <img src="${arrayOfAlbum.cover_medium}" class="card-img-top flex-grow-1 imgAlbum p-2 object-fit-cover rounded-3 img-fluid" alt="Cover Album"  />
                  <div class="card-body p-0 py-1 ">
                   <h5 class="card-title text-white pt-3 mb-4" >${arrayOfAlbum.title}</h5>
                   <p class="card-text text-white-50 p-0 mt-2 position-absolute bottom-0 ">${arrayOfAlbum.artist.name}</p>
                  </div>
                 </div>
-              
-            
+               
             `;
-        div.classList.add("col");
-
+        div.className = "col col-12 col-sm-6 col-md-4 col-lg-2 mx-auto";
         rowAlbum.appendChild(div);
       })
 
