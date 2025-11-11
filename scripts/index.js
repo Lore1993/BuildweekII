3b6c9c91d01b1cf49fe8f286105870310b835718
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-11-11 09:35:48 +0100

    arrows

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
-const idAlbum1 = "230935602";
-const idAlbum2 = "46787432";
-const idAlbum3 = "568120942";
-const idAlbum4 = "399123947";
-const idAlbum5 = "80274812";
+const idAlbum1 = "230935602"
+const idAlbum2 = "46787432"
+const idAlbum3 = "568120942"
+const idAlbum4 = "399123947"
+const idAlbum5 = "80274812"
 
 const arrayIdAlbum = [
   "245956", //La voce del padrone
@@ -10,27 +10,27 @@ const arrayIdAlbum = [
   "301773", //Mezzanine
   "421310217", //soñao
   "80274812", //Vuelves a Empezar
-];
+]
 
-const baseUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+const baseUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/"
 
 const addAlbumHomepage = function () {
   arrayIdAlbum.forEach((id) => {
-    console.log("aaa", baseUrl + id);
+    console.log("aaa", baseUrl + id)
     fetch(baseUrl + id)
       .then((res) => {
-        console.log("RESPONSE", res);
+        console.log("RESPONSE", res)
         if (res.ok) {
-          return res.json();
+          return res.json()
         } else {
           throw new Error(
             `Errore nella risposta ricevuta dal server: ${res.status}`
-          );
+          )
         }
       })
       .then((arrayOfAlbum) => {
-        const rowAlbum = document.getElementById("rowAlbum");
-        const div = document.createElement("div");
+        const rowAlbum = document.getElementById("rowAlbum")
+        const div = document.createElement("div")
 
         div.innerHTML = `
               <div class="card bg-dark mx-auto my-2 h-100">
@@ -41,16 +41,16 @@ const addAlbumHomepage = function () {
               </div>
             </div>
                
-            `;
-        div.className = "col col-12 col-sm-6 col-md-6 col-lg-3 mx-auto";
-        rowAlbum.appendChild(div);
+            `
+        div.className = "col col-12 col-sm-6 col-md-6 col-lg-2 mx-auto"
+        rowAlbum.appendChild(div)
       })
 
       .catch((err) => {
-        console.log("PROBLEMA", err);
+        console.log("PROBLEMA", err)
         // problemi di connessione, spina staccata etc.
-      });
-  });
-};
+      })
+  })
+}
 
-addAlbumHomepage();
+addAlbumHomepage()
