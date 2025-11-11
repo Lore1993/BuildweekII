a6c2ffbefb64f47a98706d4468cc2be8e9fa047f
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2025-11-11 14:24:12 +0100

    fix border radius js

--- scripts/index.js
+++ scripts/index.js
@@ -34,7 +34,7 @@ const addAlbumHomepage = function () {
 
         div.innerHTML = `
               <div class="card bg-dark mx-auto my-2 h-100">
-              <a href="./albumpage.html?albumId=${arrayOfAlbum.id}" class="text-decoration-none "><img src="${arrayOfAlbum.cover_medium}" class="card-img-top imgAlbum p-2 object-fit-cover rounded-3 img-fluid " alt="Cover Album" /></a>
+              <a href="./albumpage.html?albumId=${arrayOfAlbum.id}" class="text-decoration-none "><img src="${arrayOfAlbum.cover_medium}" class="card-img-top imgAlbum  object-fit-cover rounded-3 img-fluid " alt="Cover Album" /></a>
               <div class="card-body p-2 position-relative">
                <a href="./albumpage.html?albumId=${arrayOfAlbum.id}" class="text-decoration-none text-white"> <h5 class="card-title text-white mb-4" >${arrayOfAlbum.title}</h5></a>
                 <a href="./artistpage.html?artistId=${arrayOfAlbum.artist.id}" class="text-decoration-none text-white"><p class="card-text text-white-50 position-absolute bottom-0">${arrayOfAlbum.artist.name}</p></a>
