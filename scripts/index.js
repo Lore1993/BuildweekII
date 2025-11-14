6f82aba076a449c1c82ba8b2e9e42e697af4d0f1
Enxos27 <vincenzo.calvaruso17@gmail.com>
2025-11-14 15:21:45 +0100

    pupo 4life

--- scripts/index.js
+++ scripts/index.js
@@ -4,27 +4,27 @@ const arrayIdAlbum = [
   "301773", //Mezzanine
   "421310217", //soñao
   "80274812", //Vuelves a Empezar
-]
+];
 
-const baseUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/"
+const baseUrl = "https://striveschool-api.herokuapp.com/api/deezer/album/";
 
 const addAlbumHomepage = function () {
   arrayIdAlbum.forEach((id) => {
-    console.log("aaa", baseUrl + id)
+    console.log("aaa", baseUrl + id);
     fetch(baseUrl + id)
       .then((res) => {
-        console.log("RESPONSE", res)
+        console.log("RESPONSE", res);
         if (res.ok) {
-          return res.json()
+          return res.json();
         } else {
           throw new Error(
             `Errore nella risposta ricevuta dal server: ${res.status}`
-          )
+          );
         }
       })
       .then((arrayOfAlbum) => {
-        const rowAlbum = document.getElementById("rowAlbum")
-        const div = document.createElement("div")
+        const rowAlbum = document.getElementById("rowAlbum");
+        const div = document.createElement("div");
 
         div.innerHTML = `
               <div class="card bg-dark mx-auto my-2 h-100 artist-card">
@@ -35,74 +35,76 @@ const addAlbumHomepage = function () {
               </div>
             </div>
                
-            `
-        div.className = "col col-12 col-sm-6 col-md-6 col-lg-2 mx-auto"
-        rowAlbum.appendChild(div)
+            `;
+        div.className = "col col-12 col-sm-6 col-md-6 col-lg-2 mx-auto";
+        rowAlbum.appendChild(div);
       })
 
       .catch((err) => {
-        console.log("PROBLEMA", err)
+        console.log("PROBLEMA", err);
         // problemi di connessione, spina staccata etc.
-      })
-  })
-}
+      });
+  });
+};
 
-addAlbumHomepage()
+addAlbumHomepage();
 
 const showAlbum = function () {
-  const titolo = document.getElementById("songTitle")
-  const linkTitle = document.getElementById("linkAlbumTitle")
-  const linkImg = document.getElementById("linkAlbumImg")
-  const img = document.getElementById("cover")
-  const author = document.getElementById("author")
-  const linkauthor = document.getElementById("linkAuthor")
-  const description = document.getElementById("description")
-  const idAlbum = "12186994"
+  const titolo = document.getElementById("songTitle");
+  const linkTitle = document.getElementById("linkAlbumTitle");
+  const linkImg = document.getElementById("linkAlbumImg");
+  const img = document.getElementById("cover");
+  const author = document.getElementById("author");
+  const linkauthor = document.getElementById("linkAuthor");
+  const description = document.getElementById("description");
+  const arrayIdAlbum = ["12186994", "616950", "76353", "144718432"]; //Lucio, Pupo, Giggi,Simpatia
+  const n = Math.floor(Math.random() * 4);
+  const idAlbum = arrayIdAlbum[n];
 
   fetch(baseUrl + idAlbum)
     .then((res) => {
-      console.log("RESPONSE", res)
+      console.log("RESPONSE", res);
       if (res.ok) {
-        return res.json()
+        return res.json();
       } else {
         throw new Error(
           `Errore nella risposta ricevuta dal server: ${res.status}`
-        )
+        );
       }
     })
     .then((Album) => {
-      titolo.innerText = `${Album.title}`
-      img.src = `${Album.cover_big}`
-      author.innerText = `${Album.artist.name}`
-      description.innerText = `Ascolta il nuovo singolo di ${Album.artist.name}`
-      linkTitle.href = `./albumpage.html?albumId=${Album.id}`
-      linkImg.href = `./albumpage.html?albumId=${Album.id}`
-      linkauthor.href = `./artistpage.html?artistId=${Album.artist.id}`
+      titolo.innerText = `${Album.title}`;
+      img.src = `${Album.cover_big}`;
+      author.innerText = `${Album.artist.name}`;
+      description.innerText = `Ascolta il nuovo singolo di ${Album.artist.name}`;
+      linkTitle.href = `./albumpage.html?albumId=${Album.id}`;
+      linkImg.href = `./albumpage.html?albumId=${Album.id}`;
+      linkauthor.href = `./artistpage.html?artistId=${Album.artist.id}`;
     })
 
     .catch((err) => {
-      console.log("PROBLEMA", err)
-    })
-}
-showAlbum()
+      console.log("PROBLEMA", err);
+    });
+};
+showAlbum();
 
-const arrayIdArtist = [9822974, 180, 612, 12313080, 869356]
+const arrayIdArtist = [9822974, 180, 612, 12313080, 869356];
 
 const baseUrlArtist =
-  "https://striveschool-api.herokuapp.com/api/deezer/artist/"
+  "https://striveschool-api.herokuapp.com/api/deezer/artist/";
 
 const showArtist = function () {
-  const rowArtist = document.getElementById("row-Artist")
+  const rowArtist = document.getElementById("row-Artist");
   arrayIdArtist.forEach((Id) => {
     fetch(baseUrlArtist + Id)
       .then((res) => {
-        console.log("RESPONSE", res)
+        console.log("RESPONSE", res);
         if (res.ok) {
-          return res.json()
+          return res.json();
         } else {
           throw new Error(
             `Errore nella risposta ricevuta dal server: ${res.status}`
-          )
+          );
         }
       })
       .then((Artist) => {
@@ -125,12 +127,12 @@ const showArtist = function () {
                 <small class="text-white-50 text-start d-block"
                   >Artist</small
                 >
-              </div>`
+              </div>`;
       })
 
       .catch((err) => {
-        console.log("PROBLEMA", err)
-      })
-  })
-}
-showArtist()
+        console.log("PROBLEMA", err);
+      });
+  });
+};
+showArtist();
